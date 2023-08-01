a2fcab5757db90f5390177e6beb219b5f825e8b9
John Lucas <joohnzay@gmail.com>
2023-08-01 14:39:27 -0300

    update

--- dist/master.js
+++ dist/master.js
@@ -1,5 +1,7 @@
 "use strict";
 // Tipando Objetos
+// 1- Mostrar a cidade do usuário
+console.log('1- Mostrar a cidade do usuário');
 function showCity(user) {
     var _a;
     return (_a = user.address) === null || _a === void 0 ? void 0 : _a.city;
@@ -12,3 +14,25 @@ const response = showCity({
     }
 });
 console.log(response);
+// 2- Readonly
+console.log('2- Readonly, tá no código');
+let user = {
+    name: 'John',
+    age: 19
+};
+// 3- Extends, implements
+console.log('3- Extends, Implements. tá no código');
+class CriarVeiculo {
+    constructor(rodas) {
+        this.rodas = rodas;
+    }
+    acelerar() {
+        console.log('vrum');
+    }
+}
+// 4- Pick & Omit
+console.log('4- Pick & Omit, tá no código');
+// let postpick: PostPick
+// let postomit: PostOmit
+// postpick.id
+// postomit.description
--- src/master.ts
+++ src/master.ts
@@ -2,6 +2,8 @@
 
 // 1- Mostrar a cidade do usuário
 
+console.log('1- Mostrar a cidade do usuário')
+
 type UF = 'PE' | 'SP' | 'BA'
 
 interface User {
@@ -25,3 +27,63 @@ const response = showCity({
 })
 
 console.log(response)
+
+// 2- Readonly
+
+console.log('2- Readonly, tá no código')
+
+interface newUser {
+  readonly name: string
+  age: number
+}
+
+let user: newUser = {
+  name: 'John',
+  age: 19
+}
+
+// 3- Extends, implements
+
+console.log('3- Extends, Implements. tá no código')
+
+interface Veiculo {
+  rodas: number
+  acelerar: () => void
+  frear?: () => void
+}
+
+class CriarVeiculo implements Veiculo{
+  rodas: number
+
+  constructor(rodas:number){
+    this.rodas = rodas
+  }
+
+  acelerar(){
+    console.log('vrum')
+  }
+}
+
+ interface Moto extends Veiculo{
+   capacete: boolean
+   empinar: () => void
+}
+
+// 4- Pick & Omit
+
+console.log('4- Pick & Omit, tá no código')
+
+interface Post {
+  id: number
+  title: string
+  description: string
+}
+
+type PostPick = Pick<Post, 'id' | 'title'>
+type PostOmit = Omit<Post, 'id' | 'title'>
+
+// let postpick: PostPick
+// let postomit: PostOmit
+
+// postpick.id
+// postomit.description
