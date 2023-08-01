740c00140a2f2c198a95caeeb749d235950d2a32
John Lucas <joohnzay@gmail.com>
2023-08-01 11:57:59 -0300

    string number boolean any never void

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,34 @@
-console.log('Hello World')
+// string, number & boolean
+
+console.log('Descomente os console.log')
+
+let message: string = 'Hello World'
+
+// console.log(message)
+
+let total: number = 10.5
+
+// console.log(total)
+
+let isOpen: boolean = false
+
+// console.log(isOpen)
+
+// any, void, never
+
+let stock: any = 89
+stock = null
+
+// console.log(stock)
+
+let voidStock: void = undefined
+
+function showInformation( name: string): void{
+  console.log(name)
+}
+
+// console.log(showInformation('John'))
+
+function error(): never {
+  throw new Error('error')
+}
